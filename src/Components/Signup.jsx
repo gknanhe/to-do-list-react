d90a52bfcac596bc00652398fdd0a906f226b50b
gknanhe <cse18gknanhejd@gmail.com>
2023-11-05 14:21:13 +0530

    fixed some bug in login link click

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -89,7 +89,7 @@ const Signup = () => {
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
+                Sign up to your account
               </h1>
               <form
                 className="space-y-4 md:space-y-6"
@@ -186,7 +186,7 @@ const Signup = () => {
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Don’t have an account yet?{" "}
                   <a
-                    href="#"
+                    href="/login"
                     className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                   >
                     Sign in
